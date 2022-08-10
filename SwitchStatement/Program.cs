3a8c24cbbb80cd87f0e6ef44daab7764546c4dbb
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-10 12:40:02 -0700

    initial commit

---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("What is your favorite school subject?");
+var subject = Console.ReadLine().ToLower();
+
+switch (subject)
+{
+    case "science":
+        Console.WriteLine("You know I'm somewhat of a scientist myself.");
+        break;
+    case "math":
+        Console.WriteLine("Not my favorite but you do you.");
+        break;
+    case "music":
+        Console.WriteLine("Party on Wayne!");
+        break;
+    case "sports":
+        Console.WriteLine("If you're not first you're last!");
+        break;
+    default:
+        Console.WriteLine("Interesting....");
+        break;
+}
